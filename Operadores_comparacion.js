c5e73acbebfc599286f4ab07deeac170dce1601a
EECG1986 <estivencardona@gmail.com>
2024-02-18 18:03:09 -0500

    Operadores_comparacion.js

---
+++ Operadores_comparacion.js
@@ -0,0 +1,19 @@
+let numeroA = 10;
+console.log(numeroA);
+
+let numeroB = 20;
+console.log(numeroB);
+
+let sonIguales = numeroA == numeroB;
+console.log(sonIguales);
+
+let sonDiferentes = numeroA != numeroB;
+console.log(sonDiferentes);
+
+let tipoDato = numeroA === numeroB;
+console.log(tipoDato);
+
+let Comparacion = numeroA !== numeroB;
+console.log(Comparacion);
+
+
